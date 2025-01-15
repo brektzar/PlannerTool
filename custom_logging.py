39342d83f06db3527f92ad2a4cac2fcd75ea44ef
brektzar <64851642+brektzar@users.noreply.github.com>
2025-01-15 10:42:20 +0100

    wip logging

--- custom_logging.py
+++ custom_logging.py
@@ -119,9 +119,5 @@ def compare_and_log_changes(df, edited_data):
     if changes_made:
         for change in changes_made:
             log_action("update", 
-                       f"{st.session_state.username} ändrade uppgiften {change['column']} 
-                       \nför rad {change['index']} 
-                       \nfrån {change['old_value']} 
-                       \ntill {change['new_value']}\n", 
-                       "Planering/Redigera Mål och Uppgifter")
+                       f"{st.session_state.username} ändrade uppgiften {change['column']} \nför rad {change['index']} \nfrån {change['old_value']} \ntill {change['new_value']}\n", "Planering/Redigera Mål och Uppgifter")
     return changes_made
